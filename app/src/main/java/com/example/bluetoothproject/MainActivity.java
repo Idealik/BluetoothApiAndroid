e18d361a643b4e19d24c6cab63b8c7bdc8868cf4
idealik <shavl.mark@yandex.ru>
2019-08-05 17:46:21 +0300

    фикс багов

--- app/src/main/java/com/example/bluetoothproject/MainActivity.java
+++ app/src/main/java/com/example/bluetoothproject/MainActivity.java
@@ -267,14 +267,54 @@ public class MainActivity extends AppCompatActivity {
 
     }
 
+    public static byte[] stringToBytesUTFCustom(String str) {
+
+        char[] buffer = str.toCharArray();
+
+        byte[] b = new byte[buffer.length << 1];
+
+        for(int i = 0; i < buffer.length; i++) {
+
+            int bpos = i << 1;
+
+            b[bpos] = (byte) ((buffer[i]&0xFF00)>>8);
+
+            b[bpos + 1] = (byte) (buffer[i]&0x00FF);
+
+        }
+
+        return b;
+
+    }
+    public static String bytesToStringUTFCustom(byte[] bytes) {
+
+        char[] buffer = new char[bytes.length >> 1];
+
+        for(int i = 0; i < buffer.length; i++) {
+
+            int bpos = i << 1;
+
+            char c = (char)(((bytes[bpos]&0x00FF)<<8) + (bytes[bpos+1]&0x00FF));
+
+            buffer[i] = c;
+
+        }
+        return new String(buffer);
+
+    }
+
     private void sendData(String message) {
         byte[] msgBuffer = message.getBytes();
-
+        Log.d(TAG, "sendData: " + msgBuffer);
+        byte[] msgBuffer2 = stringToBytesUTFCustom(message);
+        Log.d(TAG, "sendData: " + msgBuffer2);
         Log.d(TAG, "...Посылаем данные: " + message + "...");
 
-        try {
-            outStream.write(msgBuffer);
+        Log.d(TAG, "REVERSE: " + bytesToStringUTFCustom(msgBuffer));
+        Log.d(TAG, "REVERSE: " + bytesToStringUTFCustom(msgBuffer2));
 
+        try {
+            outStream.write(msgBuffer2);
         } catch (IOException e) {
             if (address.equals("00:00:00:00:00:00"))
             Toast.makeText(getBaseContext(), "В переменной address у вас прописан 00:00:00:00:00:00, вам необходимо прописать реальный MAC-адрес Bluetooth модуля", Toast.LENGTH_SHORT).show();
